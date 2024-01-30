23d4ba3832641d1b32078a3ad5b781e0365b08f8
César Fernández <kaesar84@icloud.com>
2024-01-30 12:54:55 +0100

    menu

--- scripts.js
+++ scripts.js
@@ -1,4 +1,273 @@
-let palabraMinus = "hola";
+// Listas de palabras
+
+let animales = [
+  "Leon",
+  "Tigre",
+  "Elefante",
+  "Jirafa",
+  "Cebra",
+  "Oso",
+  "Mono",
+  "Koala",
+  "Panda",
+  "Lobo",
+  "Zorro",
+  "Ballena",
+  "Delfin",
+  "Aguila",
+  "Buho",
+  "Tortuga",
+  "Cocodrilo",
+  "Iguana",
+  "Rinoceronte",
+  "Hipopotamo",
+  "Gorila",
+  "Camello",
+  "Jaguar",
+  "Puma",
+];
+
+let profesiones = [
+  "Medico",
+  "Ingeniero",
+  "Profesor",
+  "Policia",
+  "Bombero",
+  "Cientifico",
+  "Arquitecto",
+  "Programador",
+  "Disenador",
+  "Artista",
+  "Periodista",
+  "Astronauta",
+  "Chef",
+  "Abogado",
+  "Enfermero",
+  "Electricista",
+  "Carpintero",
+  "Plomero",
+  "Piloto",
+  "Farmaceutico",
+  "Fotografo",
+  "Psicologo",
+  "Contador",
+  "Actor",
+];
+
+let colores = [
+  "Rojo",
+  "Azul",
+  "Verde",
+  "Amarillo",
+  "Naranja",
+  "Morado",
+  "Rosa",
+  "Blanco",
+  "Negro",
+  "Gris",
+  "Café",
+  "Celeste",
+  "Turquesa",
+  "Marrón",
+  "Beige",
+  "Ocre",
+  "Cian",
+  "Lila",
+  "Violeta",
+  "Salmon",
+  "Marfil",
+  "Turmalina",
+  "Aqua",
+  "Malva",
+  "Caoba",
+];
+
+let deportes = [
+  "Futbol",
+  "Baloncesto",
+  "Beisbol",
+  "Tenis",
+  "Atletismo",
+  "Natacion",
+  "Ciclismo",
+  "Voleibol",
+  "Golf",
+  "Hockey",
+  "Rugby",
+  "Boxeo",
+  "Esgrima",
+  "Karate",
+  "Surf",
+  "Patinaje",
+  "Automovilismo",
+  "Ajedrez",
+  "Escalada",
+  "Pesca",
+  "Softbol",
+  "Triatlon",
+  "Equitacion",
+  "Canotaje",
+  "Esqui",
+];
+
+let personajesDragonBall = [
+  "Goku",
+  "Vegeta",
+  "Piccolo",
+  "Gohan",
+  "Krillin",
+  "Bulma",
+  "Trunks",
+  "Goten",
+  "Freezer",
+  "Cell",
+  "Buu",
+  "Bills",
+  "Whis",
+  "Tenshinhan",
+  "Chaos",
+  "Yamcha",
+  "Chiaotzu",
+  "Raditz",
+  "Nappa",
+  "Pan",
+  "Bardock",
+  "Broly",
+  "Videl",
+  "Dende",
+  "Satan",
+];
+
+let personajesNaruto = [
+  "Naruto",
+  "Sasuke",
+  "Sakura",
+  "Kakashi",
+  "Hinata",
+  "Shikamaru",
+  "Gaara",
+  "Orochimaru",
+  "Jiraiya",
+  "Tsunade",
+  "Itachi",
+  "Kiba",
+  "Neji",
+  "RockLee",
+  "Ino",
+  "Choji",
+  "Deidara",
+  "Sai",
+  "Temari",
+  "Kankuro",
+  "Madara",
+  "Hashirama",
+  "Minato",
+  "Tobirama",
+];
+
+let paises = [
+  "Croacia",
+  "Canada",
+  "China",
+  "Japon",
+  "Brasil",
+  "Rusia",
+  "India",
+  "Sudafrica",
+  "Italia",
+  "Alemania",
+  "Australia",
+  "Francia",
+  "Argentina",
+  "Mexico",
+  "Egipto",
+  "Tailandia",
+  "Peru",
+  "EmiratosArabes",
+  "Singapur",
+  "CoreaDelSur",
+  "Espana",
+  "Portugal",
+  "Grecia",
+  "Noruega",
+  "Suecia",
+];
+
+let planetas = [
+  "Mercurio",
+  "Venus",
+  "Tierra",
+  "Marte",
+  "Jupiter",
+  "Saturno",
+  "Urano",
+  "Neptuno",
+];
+
+let objetosEscolares = [
+  "Lapiz",
+  "Cuaderno",
+  "Boligrafo",
+  "Regla",
+  "Tijeras",
+  "Goma",
+  "Mochila",
+  "Libro",
+  "Calculadora",
+  "Pegamento",
+  "Estuche",
+  "Papel",
+  "Marcadores",
+  "Pizarra",
+  "Compas",
+  "Pincel",
+  "Diccionario",
+  "Celo",
+  "Libro",
+  "Carpeta",
+  "Rotuladores",
+  "Sacapuntas",
+  "Agenda",
+  "Mapa",
+];
+
+let vehiculos = [
+  "Coche",
+  "Camioneta",
+  "Motocicleta",
+  "Bicicleta",
+  "Camion",
+  "Avion",
+  "Barco",
+  "Helicoptero",
+  "Tren",
+  "Tractor",
+  "Scooter",
+  "Submarino",
+  "Ambulancia",
+  "Furgoneta",
+  "Autobus",
+  "Trineo",
+  "Patines",
+  "Triciclo",
+  "Cohete",
+  "Carroza",
+  "Monopatin",
+  "Canoa",
+  "Carretilla",
+  "Jet",
+];
+
+function palabraAleatoria(array) {
+  let indice = Math.floor(Math.random() * array.length);
+  return array[indice];
+}
+
+let arraySeleccionado = animales;
+let randomP = palabraAleatoria(arraySeleccionado);
+console.log(randomP);
+
+let palabraMinus = randomP;
 let palabra = palabraMinus.toLocaleUpperCase();
 let longitudPalabra = palabra.length;
 
@@ -6,8 +275,9 @@ let copiaPalabra = new Array(longitudPalabra);
 let copiaPalabraString = copiaPalabra.toString;
 
 let zonaAbc = document.getElementById("zonaAbc");
+let contenedorBotones = document.getElementById("botonesAbecedario");
 
-let vidas = 1;
+let vidas = 5;
 let vidaReiniciar = vidas;
 let numVida = document.getElementById("numVida");
 
@@ -17,43 +287,96 @@ contadorVidas(vidas);
 // reset
 let reset = document.getElementById("reset");
 
+// home
+let home = document.getElementById("home");
+let background = document.getElementById("background");
+
+let btnJug1 = document.getElementById("btnJug1");
+
+let tituloModal = document.getElementById("tituloModal");
+let zonaSeleccionModo = document.getElementById("zonaSeleccionModo");
+let zonaSeleccionTema = document.getElementById("zonaSeleccionTema");
+
+// Listeners
+document.addEventListener("DOMContentLoaded", start());
+reset.addEventListener("click", reiniciar);
+home.addEventListener("click", modalHome);
+btnJug1.addEventListener("click", Jugador1);
+
+// modal Home
+function modalHome() {
+  background.style.display = "flex";
+}
+function Jugador1() {
+  zonaSeleccionModo.style.display = "none";
+  zonaSeleccionTema.style.display = "flex";
+  tituloModal.textContent = "Selecciona un tema";
+}
+
+let botonesTema = document.querySelectorAll(".btnTema");
+
+botonesTema.forEach(function (boton) {
+  boton.addEventListener("click", function () {
+    let valorBtn = boton.value;
+    console.log(valorBtn)
+  });
+});
+
 // contador vidas
 function contadorVidas(vida) {
-  if (vida == 9) {
-    numVida.textContent = "counter_9";
-  } else if (vida == 8) {
-    numVida.textContent = "counter_8";
-  } else if (vida == 7) {
-    numVida.textContent = "counter_7";
-  } else if (vida == 6) {
-    numVida.textContent = "counter_6";
-  } else if (vida == 5) {
-    numVida.textContent = "counter_5";
-  } else if (vida == 4) {
-    numVida.textContent = "counter_4";
-  } else if (vida == 3) {
-    numVida.textContent = "counter_3";
-  } else if (vida == 2) {
-    numVida.textContent = "counter_2";
-  } else if (vida == 1) {
-    numVida.textContent = "counter_1";
-  } else if (vida == 0) {
-    numVida.textContent = "counter_0";
+  if (vida > 0) {
+    if (vida == 9) {
+      numVida.textContent = "counter_9";
+    } else if (vida == 8) {
+      numVida.textContent = "counter_8";
+    } else if (vida == 7) {
+      numVida.textContent = "counter_7";
+    } else if (vida == 6) {
+      numVida.textContent = "counter_6";
+    } else if (vida == 5) {
+      numVida.textContent = "counter_5";
+    } else if (vida == 4) {
+      numVida.textContent = "counter_4";
+    } else if (vida == 3) {
+      numVida.textContent = "counter_3";
+    } else if (vida == 2) {
+      numVida.textContent = "counter_2";
+    } else if (vida == 1) {
+      numVida.textContent = "counter_1";
+    } else if (vida == 0) {
+      numVida.textContent = "counter_0";
+    }
+  } else {
     reset.textContent = "restart_alt";
   }
 }
 
 function reiniciar() {
   vidas = vidaReiniciar;
+  contadorVidas(vidas);
+  contenedorBotones.innerHTML = "";
+
+  randomP = palabraAleatoria(arraySeleccionado);
+  console.log(randomP);
+
+  palabraMinus = randomP;
+  palabra = palabraMinus.toLocaleUpperCase();
+  longitudPalabra = palabra.length;
+
+  copiaPalabra = new Array(longitudPalabra);
+  copiaPalabraString = copiaPalabra.toString;
+  reset.textContent = "";
+
+  buscada();
+  start();
 }
 
 // botones abecedario
-document.addEventListener("DOMContentLoaded", function () {
+function start() {
   rellenaArray(0, "*");
   buscada();
   // Obtener el contenedor donde se agregarán los botones
-  var contenedorBotones = document.getElementById("botonesAbecedario");
-
+  // var contenedorBotones = document.getElementById("botonesAbecedario");
   // Crear un array con las letras del abecedario
   var letrasAbecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
@@ -70,7 +393,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     contenedorBotones.appendChild(btnLetra);
   });
-});
+}
 
 // genera la palabra a buscar
 function buscada() {
@@ -104,9 +427,16 @@ function cambioAcierto() {
 function cambioError() {
   let palabraOk = document.querySelectorAll(".btnBuscada");
 
+  // zona palabra
   palabraOk.forEach(function (element) {
     element.classList.add("palabraError");
     element.classList.remove("btnBuscada");
+  });
+  // zona abecedario
+  let btnLetra = document.querySelectorAll(".btnLetra");
+  btnLetra.forEach(function (element) {
+    element.classList.add("btnLetraHide");
+    element.classList.remove("btnLetra");
   });
 }
 
@@ -126,6 +456,7 @@ function validarLetraIntroducida(letra) {
     if (palabra == copiaPalabra.join("")) {
       console.log("has Acertado");
       cambioAcierto();
+      reset.textContent = "restart_alt";
       break;
     }
   }
@@ -136,8 +467,8 @@ function validarLetraIntroducida(letra) {
 
   if (vidas < 1) {
     cambioError();
-    zonaAbc.style.display = "none";
-    alert("loser");
+    numVida.textContent = "counter_0";
+    // alert("loser");
   }
 }
 
@@ -151,3 +482,5 @@ function rellenaArray(index, element) {
     }
   }
 }
+
+//efecto confeti
